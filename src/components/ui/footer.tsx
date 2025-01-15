5b6190f5d31c3cff3d34a5350c92588555fc5225
TrzasQ <przytrzask@gmail.com>
2025-01-15 08:43:08 +0100

    F

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <footer className="w-full  bg-black px-8 py-16 z-10">
+    <footer className="w-full  bg-zinc-900 px-8 py-16 z-10">
       <div className="container flex items-center justify-between">
         <p className="text-sm text-muted-foreground">
           © 2025 Your Company. All rights reserved.
